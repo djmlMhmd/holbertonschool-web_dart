23a423e63a87045011a40fcddaba7c0425cbf8b6
djmlMhmd <djamaldine-mohamed@live.fr>
2025-11-14 09:27:57 +0100

    Update print statement in 5-assertion.dart to include newline for improved output formatting

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -2,5 +2,5 @@ void main(List<String> args) {
   assert(args.isNotEmpty, 'Usage: dart 5-assertion.dart <score>');
   int score = int.parse(args[0]);
   assert(score >= 80, 'The score must be bigger or equal to 80');
-  print('You Passed');
+  print('You Passed\n');
 }
