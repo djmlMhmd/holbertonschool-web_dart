a020a2634d0c8d7b31a24619770e247b69bcfe63
djmlMhmd <djamaldine-mohamed@live.fr>
2025-11-28 10:01:40 +0100

    Enhance password validation in 3-private_password.dart by adding checks for length and character requirements, and override toString method for better output representation.

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -12,4 +12,19 @@ class Password {
   }
 
   bool isValid() {
-    if (_password.length < 8 || _password.length >
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+
+    bool hasUpper = _password.contains(RegExp(r'[A-Z]'));
+    bool hasLower = _password.contains(RegExp(r'[a-z]'));
+    bool hasDigit = _password.contains(RegExp(r'[0-9]'));
+
+    return hasUpper && hasLower && hasDigit;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is $_password";
+  }
+}
